bc39dfab7e32b3c3abb89369774e669acfd28f91
tsypnyatov-sergey <tsypnyatov2012@gmail.com>
2026-09-05 10:47:56 +0300

    add api app, connected apps fix: media and static urls

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from config import settings
 
 APPS_URLS =[
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/orders/', include('orders.urls')),
+    path('api/products/', include('products.urls')),
+    path('api/reviews/', include('reviews.urls')),
+    path('shop/', include('shop.urls')),
+
 ]
 
 MEDIA_URLS = (
@@ -13,4 +19,7 @@ MEDIA_URLS = (
         + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
 
-urlpatterns = APPS_URLS + MEDIA_URLS
+urlpatterns = APPS_URLS
+
+if settings.DEBUG:
+    urlpatterns += MEDIA_URLS
